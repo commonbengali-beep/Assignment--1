85968f64c04ffacd12d5268d8e9e2f00bcce677d
commonbengali-beep <commonbengali@gmail.com>
2026-09-14 19:38:14 +0600

    Add files via upload

---
+++ Bubble.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void bSort(int arr[], int n) {
+    int i, j, temp;
+
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+int main() {
+    int arr[100], n, i;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    bSort(arr, n);
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
